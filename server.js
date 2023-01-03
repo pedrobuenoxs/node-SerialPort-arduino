b476dc2bc72e64506e4e3dbd2a9138216207b65c
pedrobuenoxs <pedrohbueno02@gmail.com>
2023-01-03 20:12:23 -0300

    cleaning branch

--- server.js
+++
@@ -1,8 +0,0 @@
-import { server } from "./app/config/app.js";
-import * as dotenv from "dotenv";
-dotenv.config();
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
